6f3572cee58d1d4a5b225c55c12e672fb4bdfbd1
Matheus Mignoni <matheus.mignoni@poatek.com>
2019-10-25 23:11:02 -0300

    Added IoC and Logging Configuration

---
+++ DataAnalyzer/Configuration/IocContainerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAnalyzer.Configuration
+{
+    public static class IocContainerConfiguration
+    {
+        public static void ConfigureService(IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.AddTransient<IDataAnalyzerApp, DataAnalyzerApp>();
+
+        }
+    }
+}
---
+++ DataAnalyzer/Configuration/LoggingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DataAnalyzer.Configuration
+{
+    public static class LoggingConfiguration
+    {
+        public static void ConfigureService(IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.AddLogging(configure =>
+            {
+                configure.AddConfiguration(configuration.GetSection("Logging"));
+                configure.AddConsole();
+            });
+        }
+    }
+}
